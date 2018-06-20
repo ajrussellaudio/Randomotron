644c51ff0a445537332338886c4a0ef7a8ead20f
Alan Russell <ajrussellaudio@gmail.com>
2018-06-20 22:47:49 +0100

    install mongodb

--- server.js
+++ server.js
@@ -2,10 +2,21 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/api/groups", (req, res, next) => {
-  res.send(`cohort.groups(${req.query.size})`);
-});
+const MongoClient = require("mongodb").MongoClient;
 
-app.listen(port, () => {
-  console.log(`App listening on ${port}`);
-});
+MongoClient.connect(
+  "mongodb://localhost:27017",
+  (err, client) => {
+    if (err) next(err);
+    const db = client.db("randomotron");
+    console.log("Connected to db...");
+
+    app.get("/api/groups", (req, res, next) => {
+      res.send(`cohort.groups(${req.query.size})`);
+    });
+
+    app.listen(port, () => {
+      console.log(`App listening on ${port}`);
+    });
+  }
+);
