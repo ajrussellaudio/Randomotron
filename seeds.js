69dc3b98c82135aa5b96d51945d5b0a96477cb46
Alan Russell <ajrussellaudio@gmail.com>
2018-06-20 23:07:15 +0100

    add seeds

---
+++ seeds.js
@@ -0,0 +1,21 @@
+const MongoClient = require("mongodb").MongoClient;
+const Student = require("./models/Student");
+
+MongoClient.connect(
+  "mongodb://localhost:27017",
+  (err, client) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    const db = client.db("randomotron");
+    const cohort = db.collection("cohort");
+    cohort.remove({}, () => console.log("Previous data removed"));
+
+    const studentNames = ["Alan", "Bob", "Charlie", "Dave"];
+    cohort.insert(studentNames.map(name => new Student(name)), () => {
+      console.log("Database successfully seeded");
+      client.close();
+    });
+  }
+);
