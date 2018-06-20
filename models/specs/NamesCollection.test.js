78532c7a951852a01edf5529662940a81beb1734
Alan Russell <ajrussellaudio@gmail.com>
2018-06-20 21:18:06 +0100

    add acceptance criteria

---
+++ models/specs/NamesCollection.test.js
@@ -0,0 +1,8 @@
+const NamesCollection = require("../NamesCollection");
+
+describe("NamesCollection", () => {
+  it("starts empty");
+  it("adds a name");
+  it("returns a random name");
+  it("returns an array of a given length, containing random names");
+})
