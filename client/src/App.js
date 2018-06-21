b4dd31d9de2358cf293d5244d3e852e9f51e3b87
Alan Russell <ajrussellaudio@gmail.com>
2018-06-21 23:31:55 +0100

    add dummy front page to make sure this works

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,31 @@
 import React, { Component } from "react";
 
 class App extends Component {
-  render() {
+  state = {
+    students: []
+  };
+
+  componentDidMount = () => {
+    fetch("/api/students")
+      .then(res => res.json())
+      .then(students => this.setState({ students: students }));
+  };
+
+  render = () => {
+    const students = this.state.students.map(student => (
+      <li>
+        <p>
+          <strong>{student.name}</strong>, {student.cohort}
+        </p>
+      </li>
+    ));
     return (
       <div className="App">
         <h1>Random-O-Tron</h1>
+        <ul>{students}</ul>
       </div>
     );
-  }
+  };
 }
 
 export default App;
